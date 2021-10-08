d014a7e56bcc5479941338c2419a33471229aa51
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-10-08 23:01:29 +0300

    Added UserController.

---
+++ ConversationOverflow/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using ConnectToDB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConversationOverflow.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IServiceProvider _service;
+        private readonly ConversationOverflowDbContext _conversationOverflowDbContext;
+
+        public UserController(ILogger<WeatherForecastController> logger, IServiceProvider service)
+        {
+            _logger = logger;
+            _service = service;
+            _conversationOverflowDbContext = service.GetService(typeof(ConversationOverflowDbContext)) as ConversationOverflowDbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _conversationOverflowDbContext.Users;
+        }
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return _conversationOverflowDbContext.Users.FirstOrDefault(user => user.Id == id);
+        }
+    }
+}
