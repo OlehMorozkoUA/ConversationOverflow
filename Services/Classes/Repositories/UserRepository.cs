7afd8a74205484476e9c7bc65044c1f89ada6291
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-10-08 22:20:37 +0300

    Added UserRepository.

---
+++ Services/Classes/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using ConnectToDB;
+using Microsoft.EntityFrameworkCore;
+using Models.Classes;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Classes.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ConversationOverflowDbContext _conversationOverflowDbContext;
+        public UserRepository(ConversationOverflowDbContext conversationOverflowDbContext)
+        {
+            _conversationOverflowDbContext = conversationOverflowDbContext;
+        }
+        public async Task<User> CreateUserAsync(User user)
+        {
+            _conversationOverflowDbContext.Add(user);
+            await _conversationOverflowDbContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<List<User>> GetAllUserAsync()
+        {
+            return await _conversationOverflowDbContext.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _conversationOverflowDbContext.Users.FirstOrDefaultAsync(user => user.Id == id);
+        }
+    }
+}
---
+++ Services/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllUserAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> CreateUserAsync(User user);
+
+    }
+}
