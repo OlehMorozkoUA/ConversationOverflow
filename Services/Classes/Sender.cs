c799bb01beec528c3cf9becae40525cf3a24be4b
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-10-13 00:39:39 +0300

    Added Sender(SMTP)

---
+++ Services/Classes/Sender.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Services.Classes
+{
+    public static class Sender
+    {
+        private static string email = "########################";
+        private static string password = "#########################";
+        private static string host = "smtp-mail.outlook.com";
+        private static int port = 587;
+        private static bool enableSsl = true;
+        private static int timeout = 10000;
+
+        public static void Send(string tomail, string subject, string body)
+        {
+            SmtpClient client = new SmtpClient(host, port);
+            client.EnableSsl = enableSsl;
+            client.Timeout = timeout;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+
+            client.Credentials = new NetworkCredential(email, password);
+
+            MailMessage msg = new MailMessage(email, tomail, subject, body);
+            msg.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            client.Send(msg);
+        }
+    }
+}
