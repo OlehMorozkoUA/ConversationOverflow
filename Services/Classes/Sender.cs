12a12ecdd12aa374ac82b55ef80800fce75e5c8b
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-10-13 15:46:50 +0300

    Sender

--- Services/Classes/Sender.cs
+++ Services/Classes/Sender.cs
@@ -1,18 +1,19 @@
 ﻿using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace Services.Classes
 {
     public static class Sender
     {
-        private static string email = "########################";
-        private static string password = "#########################";
+        private static string email = "conversationoverflow@outlook.com";
+        private static string password = "############################";
         private static string host = "smtp-mail.outlook.com";
         private static int port = 587;
         private static bool enableSsl = true;
         private static int timeout = 10000;
 
-        public static void Send(string tomail, string subject, string body)
+        public static async Task Send(string tomail, string subject, string body)
         {
             SmtpClient client = new SmtpClient(host, port);
             client.EnableSsl = enableSsl;
@@ -25,7 +26,7 @@ namespace Services.Classes
             MailMessage msg = new MailMessage(email, tomail, subject, body);
             msg.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
 
-            client.Send(msg);
+            await client.SendMailAsync(msg);
         }
     }
 }
