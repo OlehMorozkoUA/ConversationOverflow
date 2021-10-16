accc7dbd4bbf76d969740732b115476ac58e1d67
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-10-16 16:20:48 +0300

    Added Migration.

---
+++ ConnectToDB/Migrations/20211016124340_LocationMigration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConnectToDB.Migrations
+{
+    public partial class LocationMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Country = table.Column<string>(nullable: true),
+                    Region = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Postcode = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_LocationId",
+                table: "Users",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Locations_LocationId",
+                table: "Users",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Locations_LocationId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_LocationId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Users");
+        }
+    }
+}
---
+++ ConnectToDB/Migrations/20211016124804_LocationMigration2.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConnectToDB.Migrations
+{
+    public partial class LocationMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Locations_LocationId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_LocationId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Locations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_UserId",
+                table: "Locations",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Locations_Users_UserId",
+                table: "Locations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Locations_Users_UserId",
+                table: "Locations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Locations_UserId",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Locations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_LocationId",
+                table: "Users",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Locations_LocationId",
+                table: "Users",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
