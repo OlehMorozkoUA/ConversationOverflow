2fbed7bf5d63bad4e3f300317e4e820e361fd2a2
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-10-16 20:13:45 +0300

    Deleted Field Password.

---
+++ ConnectToDB/Migrations/20211016143235_IdentityMigration2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConnectToDB.Migrations
+{
+    public partial class IdentityMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
