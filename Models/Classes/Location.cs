bfddea2f44e7bd8e925bfb5540ae851ac3a6b365
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-10-16 16:22:05 +0300

    Added Location.

---
+++ Models/Classes/Location.cs
@@ -0,0 +1,14 @@
+﻿using Models.Interfaces;
+
+namespace Models.Classes
+{
+    public class Location : ILocation 
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string Address { get; set; }
+        public int Postcode { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Models/Interfaces/ILocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Interfaces
+{
+    interface ILocation : IIdentity<int>
+    {
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string Address { get; set; }
+        public int Postcode { get; set; }
+        public int UserId { get; set; }
+    }
+}
