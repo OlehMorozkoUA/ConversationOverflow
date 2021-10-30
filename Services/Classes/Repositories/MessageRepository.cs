0c33a61de4613731efb8d5b778333e50de588839
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-10-30 21:35:05 +0300

    Added message repository

---
+++ Services/Classes/Repositories/MessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Classes.Repositories
+{
+    public class MessageRepository : IMessageRepository
+    {
+
+    }
+}
