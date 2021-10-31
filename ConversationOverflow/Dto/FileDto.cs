abf5cdf7795f20c139171c10c94eefe197d0df58
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-10-31 16:48:55 +0200

    Added FileDto

---
+++ ConversationOverflow/Dto/FileDto.cs
@@ -0,0 +1,17 @@
+﻿using ConversationOverflow.Attributes;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConversationOverflow.Dto
+{
+    public class FileDto
+    {
+        [Required(ErrorMessage = "Please select a file.")]
+        [DataType(DataType.Upload)]
+        [MaxFileSize(5 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".jpg", ".png" })]
+        public IFormFile Image { get; set; }
+        [Required]
+        public string FilePath { get; set; }
+    }
+}
