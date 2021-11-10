b7a8d004395711ffdd1ff5263d3128499d40f29c
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-11-10 18:18:00 +0200

    Added LocationRepository

---
+++ Services/Classes/Repositories/LocationRepository.cs
@@ -0,0 +1,45 @@
+﻿using ConnectToDB;
+using Microsoft.EntityFrameworkCore;
+using Models.Classes;
+using Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace Services.Classes.Repositories
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly ConversationOverflowDbContext _conversationOverflowDbContext;
+        public LocationRepository(ConversationOverflowDbContext conversationOverflowDbContext)
+        {
+            _conversationOverflowDbContext = conversationOverflowDbContext;
+        }
+        public async Task<Location> GetLocationByUserIdAsync(int userId)
+            => await _conversationOverflowDbContext.Locations.FirstOrDefaultAsync(location => location.UserId == userId);
+
+        public async Task AddLocationAsync(int userId, Location location)
+        {
+            Location loc = await GetLocationByUserIdAsync(userId);
+
+            if (loc == null)
+            {
+                location.UserId = userId;
+                await _conversationOverflowDbContext.Locations.AddAsync(location);
+                await _conversationOverflowDbContext.SaveChangesAsync();
+            }
+        }
+        
+        public async Task UpdateLocationAsync(int userId, Location location)
+        {
+            Location loc = await GetLocationByUserIdAsync(userId);
+
+            if (loc != null)
+            {
+                loc.Country = location.Country;
+                loc.Region = location.Region;
+                loc.Address = location.Address;
+                loc.Postcode = location.Postcode;
+                await _conversationOverflowDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Services/Interfaces/ILocationRepository.cs
@@ -0,0 +1,12 @@
+﻿using Models.Classes;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ILocationRepository
+    {
+        Task<Location> GetLocationByUserIdAsync(int userId);
+        Task AddLocationAsync(int userId, Location location);
+        Task UpdateLocationAsync(int userId, Location location);
+    }
+}
