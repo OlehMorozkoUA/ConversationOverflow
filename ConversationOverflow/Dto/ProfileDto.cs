18b4108ec323e046ce260a5c7483ea0015054e1c
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-11-10 18:21:32 +0200

    Added ProfileDto

---
+++ ConversationOverflow/Dto/ProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConversationOverflow.Dto
+{
+    public class ProfileDto
+    {
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime Birthday { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+    }
+}
