dee9bd0e8c4d8b695fcf5bc2622fe5718ee1be04
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-11-10 18:18:28 +0200

    Added GroupRepository

---
+++ Services/Classes/Repositories/GroupRepository.cs
@@ -0,0 +1,24 @@
+﻿using ConnectToDB;
+using Microsoft.EntityFrameworkCore;
+using Models.Classes;
+using Services.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Classes.Repositories
+{
+    public class GroupRepository : IGroupRepository
+    {
+        private readonly ConversationOverflowDbContext _conversationOverflowDbContext;
+        public GroupRepository(ConversationOverflowDbContext conversationOverflowDbContext)
+        {
+            _conversationOverflowDbContext = conversationOverflowDbContext;
+        }
+
+        public async Task AddGroupAsync(Group group)
+            => await _conversationOverflowDbContext.AddAsync(group);
+
+        public async Task<Group> GetGroupByIdAsync(int id)
+            => await _conversationOverflowDbContext.Groups.FirstOrDefaultAsync(group => group.Id == id);
+    }
+}
---
+++ Services/Interfaces/IGroupRepository.cs
@@ -0,0 +1,11 @@
+﻿using Models.Classes;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IGroupRepository
+    {
+        Task AddGroupAsync(Group group);
+        Task<Group> GetGroupByIdAsync(int id);
+    }
+}
