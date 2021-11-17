a2e0cfd21e80edae0d2e948fed56dd13c8c57585
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-11-17 18:36:00 +0200

    Added GroupController

---
+++ ConversationOverflow/Controllers/GroupController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models.Classes;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConversationOverflow.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class GroupController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IGroupRepository _groups;
+
+        public GroupController(ILogger<UserController> logger,
+            IGroupRepository groups)
+        {
+            _logger = logger;
+            _groups = groups;
+        }
+
+        [HttpGet]
+        public async Task<List<Group>> Get() => await _groups.GetAllGroupsAsync(User.Identity.Name);
+        [HttpPost]
+        public async Task Post(Group group) => await _groups.AddGroupAsync(User.Identity.Name, group);
+        [HttpPut]
+    }
+}
---
+++ ConversationOverflowMVC/Controllers/GroupController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConversationOverflowMVC.Controllers
+{
+    public class GroupController : Controller
+    {
+        public IActionResult List()
+        {
+            ViewData["Title"] = "List Group";
+            return View();
+        }
+    }
+}
