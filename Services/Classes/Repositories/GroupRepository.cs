2396a041d0794af74986ee16abc2784f1e84e62e
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-11-17 18:37:28 +0200

    Added GroupRepository

--- Services/Classes/Repositories/GroupRepository.cs
+++ Services/Classes/Repositories/GroupRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Models.Classes;
 using Services.Interfaces;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,10 +16,34 @@ namespace Services.Classes.Repositories
             _conversationOverflowDbContext = conversationOverflowDbContext;
         }
 
-        public async Task AddGroupAsync(Group group)
-            => await _conversationOverflowDbContext.AddAsync(group);
+        public async Task AddGroupAsync(string login, Group group)
+        {
+            User user = _conversationOverflowDbContext.Users.AsQueryable().Where(user => user.Login == login).FirstOrDefaultAsync().Result;
+
+            await _conversationOverflowDbContext.Groups.AddAsync(group);
+            await _conversationOverflowDbContext.GroupUsers.AddAsync(new GroupUser()
+            {
+                UserId = user.Id,
+                GroupId = group.Id
+            });
+        }
+
+        public async Task<List<Group>> GetAllGroupsAsync(string login)
+            => await _conversationOverflowDbContext.GroupUsers.AsQueryable()
+            .Include(groupUser => groupUser.Group)
+            .Include(groupUser => groupUser.User)
+            .Where(groupUser => groupUser.User.Login == login)
+            .Select(groupUser => groupUser.Group)
+            .ToListAsync();
 
         public async Task<Group> GetGroupByIdAsync(int id)
             => await _conversationOverflowDbContext.Groups.FirstOrDefaultAsync(group => group.Id == id);
+
+        public async Task Update(string login, int groupId, Group group)
+        {
+            User user = _conversationOverflowDbContext.Users.AsQueryable().Where(user => user.Login == login).FirstOrDefaultAsync().Result;
+
+            
+        }
     }
 }
--- Services/Interfaces/IGroupRepository.cs
+++ Services/Interfaces/IGroupRepository.cs
@@ -1,11 +1,14 @@
 ﻿using Models.Classes;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Services.Interfaces
 {
     public interface IGroupRepository
     {
-        Task AddGroupAsync(Group group);
+        Task AddGroupAsync(string login, Group group);
+        Task<List<Group>> GetAllGroupsAsync(string login);
         Task<Group> GetGroupByIdAsync(int id);
+        Task Update(string login, int groupId, Group group);
     }
 }
