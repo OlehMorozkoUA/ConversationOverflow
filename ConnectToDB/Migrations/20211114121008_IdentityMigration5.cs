e1f6d693d489c4b99cb8dd69c7624833de5d3505
OlehMorozkoUA <oleh.morozko@gmail.com>
2021-11-17 18:34:58 +0200

    new migration

---
+++ ConnectToDB/Migrations/20211114121008_IdentityMigration5.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConnectToDB.Migrations
+{
+    public partial class IdentityMigration5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
